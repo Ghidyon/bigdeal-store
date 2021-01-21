f6480a078e0fa2f136362b8caedd61c33f43fdbb
Gideon Akunana <gideonakunana@gmail.com>
2021-01-21 03:15:40 +0100

    added register.php

--- home.php
+++ home.php
@@ -5673,163 +5673,9 @@
 </section>
 <!--contact banner end-->
 
-<!--footer start-->
-<footer class="footer-2">
-  <div class="container ">
-    <div class="row">
-      <div class="col-12">
-        <div class="footer-main-contian">
-          <div class="row ">
-            <div class="col-lg-4 col-md-12 ">
-              <div class="footer-left">
-                <div class="footer-logo">
-                  <img src="<?=ROOT?>assets/images/layout-2/logo/logo.png" class="img-fluid  " alt="logo">
-                </div>
-                <div class="footer-detail">
-                  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</p>
-                  <ul class="paymant-bottom">
-                    <li><a href="#"><img src="<?=ROOT?>assets/images/layout-1/pay/1.png" class="img-fluid" alt="pay"></a></li>
-                    <li><a href="#"><img src="<?=ROOT?>assets/images/layout-1/pay/2.png" class="img-fluid" alt="pay"></a></li>
-                    <li><a href="#"><img src="<?=ROOT?>assets/images/layout-1/pay/3.png" class="img-fluid" alt="pay"></a></li>
-                    <li><a href="#"><img src="<?=ROOT?>assets/images/layout-1/pay/4.png" class="img-fluid" alt="pay"></a></li>
-                    <li><a href="#"><img src="<?=ROOT?>assets/images/layout-1/pay/5.png" class="img-fluid" alt="pay"></a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-8 col-md-12 ">
-              <div class="footer-right">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="subscribe-section">
-                      <div class="row">
-                        <div class="col-md-5 ">
-                          <div class="subscribe-block">
-                            <div class="subscrib-contant ">
-                              <h4>subscribe to newsletter</h4>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-7 ">
-                          <div class="subscribe-block">
-                            <div class="subscrib-contant">
-                              <div class="input-group" >
-                                <div class="input-group-prepend">
-                                  <span class="input-group-text" ><i class="fa fa-envelope-o" ></i></span>
-                                </div>
-                                <input type="text" class="form-control" placeholder="your email" >
-                                <div class="input-group-prepend">
-                                  <span class="input-group-text telly" ><i class="fa fa-telegram" ></i></span>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class=account-right>
-                      <div class="row">
-                        <div class="col-md-4">
-                          <div class="footer-box">
-                            <div class="footer-title">
-                              <h5>my account</h5>
-                            </div>
-                            <div class="footer-contant">
-                              <ul>
-                                <li><a href="#">about us</a></li>
-                                <li><a href="#">contact us</a></li>
-                                <li><a href="#">terms & conditions</a></li>
-                                <li><a href="#">returns & exchanges</a></li>
-                                <li><a href="#">shipping & delivery</a></li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-3">
-                          <div class="footer-box">
-                            <div class="footer-title">
-                              <h5>quick link</h5>
-                            </div>
-                            <div class="footer-contant">
-                              <ul>
-                                <li><a href="#">store location</a></li>
-                                <li><a href="#"> my account</a></li>
-                                <li><a href="#"> orders tracking</a></li>
-                                <li><a href="#"> size guide</a></li>
-                                <li><a href="#">FAQ </a></li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-5">
-                          <div class="footer-box footer-contact-box">
-                            <div class="footer-title">
-                              <h5>contact us</h5>
-                            </div>
-                            <div class="footer-contant">
-                              <ul class="contact-list">
-                                <li><i class="fa fa-map-marker"></i><span>big deal store<br> <span> California-3654123</span></span></li>
-                                <li><i class="fa fa-phone"></i><span>call us: +1-857-240-5791</span></li>
-                                <li><i class="fa fa-envelope-o"></i><span>email us: support@bigdeal.com</span></li>
-                                <li><i class="fa fa-fax"></i><span>fax 123456</span></li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="app-link-block  bg-transparent">
-    <div class="container">
-      <div class="row">
-        <div class="app-link-bloc-contain app-link-bloc-contain-1">
-          <div class="app-item-group">
-            <div class="app-item">
-              <img src="<?=ROOT?>assets/images/layout-1/app/1.png" class="img-fluid" alt="app-banner">
-            </div>
-            <div class="app-item">
-              <img src="<?=ROOT?>assets/images/layout-1/app/2.png" class="img-fluid" alt="app-banner">
-            </div>
-          </div>
-          <div class="app-item-group ">
-            <div class="sosiyal-block" >
-              <h6>follow us</h6>
-              <ul class="sosiyal">
-                <li><a href="#"><i class="fa fa-facebook" ></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus" ></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter" ></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram" ></i></a></li>
-                <li><a href="#"><i class="fa fa-rss" ></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="sub-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="sub-footer-contain">
-            <p><span>2018 - 19 </span>copy right by themeforest powered by pixel strap</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
-<!--footer end-->
+<?php 
+    require_once(APP_ROOT . DS . 'includes' . DS . 'footer.php');
+?>
 
 
 <!-- tap to top -->
@@ -6034,7 +5880,7 @@
       </div>
       <div>
         <h5 class="forget-class"><a href="forget-pwd.html" class="d-block">forgot password?</a></h5>
-        <h6 class="forget-class"><a href="register.html" class="d-block">new to store? Signup now</a></h6>
+        <h6 class="forget-class"><a href="register" class="d-block">new to store? Signup now</a></h6>
       </div>
     </form>
   </div>
@@ -6184,30 +6030,6 @@
 </div> -->
 <!-- notification product -->
 
-<!-- latest jquery-->
-<script src="<?=ROOT?>assets/js/jquery-3.3.1.min.js"></script>
-
-<!-- slick js-->
-<script src="<?=ROOT?>assets/js/slick.js"></script>
-
-<!-- popper js-->
-<script src="<?=ROOT?>assets/js/popper.min.js" ></script>
-
-<!-- Timer js-->
-<script src="<?=ROOT?>assets/js/menu.js"></script>
-
-<!-- Bootstrap js-->
-<script src="<?=ROOT?>assets/js/bootstrap.js"></script>
-
-<!-- Bootstrap js-->
-<script src="<?=ROOT?>assets/js/bootstrap-notify.min.js"></script>
-
-<!-- Theme js-->
-<script src="<?=ROOT?>assets/js/script.js"></script>
-<script src="<?=ROOT?>assets/js/slider-animat.js"></script>
-<script src="<?=ROOT?>assets/js/timer.js"></script>
-<script src="<?=ROOT?>assets/js/modal.js"></script>
-</body>
-
-<!-- Mirrored from themes.pixelstrap.com/bigdeal/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 18 Jan 2021 10:34:02 GMT -->
-</html>
+<?php
+    require_once(APP_ROOT . DS . 'includes' . DS . 'script.php');
+?>
---
+++ includes/script.php
@@ -0,0 +1,27 @@
+<!-- latest jquery-->
+<script src="<?=ROOT?>assets/js/jquery-3.3.1.min.js"></script>
+
+<!-- slick js-->
+<script src="<?=ROOT?>assets/js/slick.js"></script>
+
+<!-- popper js-->
+<script src="<?=ROOT?>assets/js/popper.min.js" ></script>
+
+<!-- Timer js-->
+<script src="<?=ROOT?>assets/js/menu.js"></script>
+
+<!-- Bootstrap js-->
+<script src="<?=ROOT?>assets/js/bootstrap.js"></script>
+
+<!-- Bootstrap js-->
+<script src="<?=ROOT?>assets/js/bootstrap-notify.min.js"></script>
+
+<!-- Theme js-->
+<script src="<?=ROOT?>assets/js/script.js"></script>
+<script src="<?=ROOT?>assets/js/slider-animat.js"></script>
+<script src="<?=ROOT?>assets/js/timer.js"></script>
+<script src="<?=ROOT?>assets/js/modal.js"></script>
+</body>
+
+<!-- Mirrored from themes.pixelstrap.com/bigdeal/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 18 Jan 2021 10:34:02 GMT -->
+</html>
